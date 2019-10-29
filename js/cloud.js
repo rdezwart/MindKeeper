d6dbfd4938ce22cd5bec5d9039e4ee82fbb3f1a8
Robin de Zwart <robindezwart11@gmail.com>
2019-10-29 16:49:00 -0700

    Skeleton setup
    Imported JQuery, Popper, Bootstrap, and Firebase.

---
+++ js/cloud.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBYQvNszBdSdjXSggqYbYYEgdQ9QT5M2kQ",
+    authDomain: "rdz-mindkeeper.firebaseapp.com",
+    databaseURL: "https://rdz-mindkeeper.firebaseio.com",
+    projectId: "rdz-mindkeeper",
+    storageBucket: "rdz-mindkeeper.appspot.com",
+    messagingSenderId: "280209283741",
+    appId: "1:280209283741:web:51c40380dc7c26ba975274",
+    measurementId: "G-Q48KLXZQXD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
