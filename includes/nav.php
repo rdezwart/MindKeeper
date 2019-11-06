06898790acbe3803828e5f420f9ba02cc7b47d42
Robin de Zwart <robindezwart11@gmail.com>
2019-11-05 20:39:49 -0800

    Reponsive navigation

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,26 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark" style="margin-bottom: 30px;">
     <span class="navbar-brand">MindKeeper</span>
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="index.php" id="index">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="list.php" id="list">List</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="journal.php" id="journal">Journal</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="account.php" id="account">Account</a>
-        </li>
-    </ul>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarToggle">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php" id="index">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="list.php" id="list">List</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="journal.php" id="journal">Journal</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="account.php" id="account">Account</a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
 <script>
